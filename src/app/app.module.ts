afb1ea1669ae851c361788365fc57211943ad523
David Israel <retrochanel97@gmail.com>
2022-05-14 18:00:19 -0500

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heros/hero/hero.component';
 import { ListComponent } from './heros/list/list.component';
+import { HeroModule } from './heros/heros.module';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, CounterComponent],
+  imports: [BrowserModule, HeroModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heros/heros.module.ts
+++ src/app/heros/heros.module.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HeroComponent } from './hero/hero.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  imports: [],
-  exports: [],
-  declarations: [HeroComponent],
+  imports: [CommonModule],
+  exports: [ListComponent],
+  declarations: [HeroComponent, ListComponent],
   providers: [],
 })
-export class HeroModule { }
+export class HeroModule {}
